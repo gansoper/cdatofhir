ae4ba85a3273739ef0ad734e26e4a4cb55e33d82
gansoper <gansoper@gmail.com>
2022-08-05 16:41:28 +0300

    Move outer libs to outer folder. Batch for adding libs to maven repo.

---
+++ external-libs/addToRepo.bat
@@ -0,0 +1,32 @@
+call mvn install:install-file -Dfile=lpg.runtime.java-2.0.17.v201004271640.jar -DgroupId=lpg.runtime -DartifactId=java -Dversion=2.0.17 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=org.eclipse.emf.common-2.13.0.v20170609-0707.jar -DgroupId=org.eclipse.emf -DartifactId=common -Dversion=2.13.0 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=org.eclipse.emf.ecore-2.13.0.v20170609-0707.jar -DgroupId=org.eclipse.emf -DartifactId=ecore -Dversion=2.13.0 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=org.eclipse.emf.ecore.xmi-2.13.0.v20170609-0707.jar -DgroupId=org.eclipse.emf.ecore -DartifactId=xmi -Dversion=2.13.0 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=org.eclipse.mdht.emf.runtime-3.0.0.202201250600.jar -DgroupId=org.eclipse.mdht.emf -DartifactId=runtime -Dversion=3.0.0 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=org.eclipse.mdht.uml.cda-3.0.0.202201250600.jar -DgroupId=org.eclipse.mdht.uml -DartifactId=cda -Dversion=3.0.0 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=org.eclipse.mdht.uml.hl7.datatypes-3.0.0.202201250600.jar -DgroupId=org.eclipse.mdht.uml.hl7 -DartifactId=datatypes -Dversion=3.0.0 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=org.eclipse.mdht.uml.hl7.rim-3.0.0.202201250600.jar -DgroupId=org.eclipse.mdht.uml.hl7 -DartifactId=rim -Dversion=3.0.0 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=org.eclipse.mdht.uml.hl7.vocab-3.0.0.202201250600.jar -DgroupId=org.eclipse.mdht.uml.hl7 -DartifactId=vocab -Dversion=3.0.0 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=org.eclipse.ocl-3.6.200.v20170522-1736.jar -DgroupId=org.eclipse -DartifactId=ocl -Dversion=3.6.200 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=org.eclipse.ocl.common-1.4.200.v20160613-1518.jar -DgroupId=org.eclipse.ocl -DartifactId=common -Dversion=1.4.200 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=org.eclipse.ocl.ecore-3.6.200.v20170522-1736.jar -DgroupId=org.eclipse.ocl -DartifactId=ecore -Dversion=3.6.200 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=org.eclipse.uml2.common-2.1.0.v20170605-1616.jar -DgroupId=org.eclipse.uml2 -DartifactId=common -Dversion=2.1.0 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=org.eclipse.uml2.types-2.0.0.v20170605-1616.jar -DgroupId=org.eclipse.uml2 -DartifactId=types -Dversion=2.0.0 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=org.openhealthtools.mdht.uml.cda.consol2-3.0.0.20220126.jar -DgroupId=org.openhealthtools.mdht.uml.cda -DartifactId=consol2 -Dversion=3.0.0 -Dpackaging=jar
+
+call mvn install:install-file -Dfile=org.openhealthtools.mdht.uml.cda.mu2consol-3.0.0.20220126.jar -DgroupId=org.openhealthtools.mdht.uml.cda -DartifactId=mu2consol -Dversion=3.0.0 -Dpackaging=jar
+
